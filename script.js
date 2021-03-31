3dc475722f61e4d703eb6dad536db991b5270d11
Paulo Thor <paulothor12345@gmail.com>
2021-03-31 14:50:47 -0300

    finalizando labirinto

--- script.js
+++ script.js
@@ -17,6 +17,8 @@ const map = [
 ];
 
 const main = document.querySelector('main')
+let playerLine = 0
+let playerColumn = 0
 
 for (let i = 0; i < map.length; i++) {
     const divLine = document.createElement('div');
@@ -24,11 +26,67 @@ for (let i = 0; i < map.length; i++) {
     main.appendChild(divLine)
     for (let j = 0; j < map[i].length; j++) {
         const div = document.createElement('div');
-        div.style.width = 40 + 'px';
-        div.style.height = 40 + 'px';
+        div.classList.add('square')
         divLine.appendChild(div);
+        div.id = `div${i}${j}`
         if (map[i][j] === 'W') {
             div.style.background = '#145399'
         }
+        if (map[i][j] === 'S') {
+            const player = document.createElement('div')
+            player.id = 'player'
+            player.classList.add('square')
+            div.appendChild(player)
+            playerLine = i;
+            playerColumn = j;
+        }
     }
 }
+
+const movePlayer = {
+  'ArrowDown' : () => {
+    if (map[playerLine+1][playerColumn] !== 'W'){
+      playerLine += 1
+    }
+  },
+  'ArrowUp' : () => {
+    if (map[playerLine-1][playerColumn] !== 'W'){
+      playerLine -= 1
+    }
+  },
+  'ArrowLeft' : () => {
+    const nextPosition = map[playerLine][playerColumn-1];
+    if (nextPosition === undefined || nextPosition === 'W') {return}
+    playerColumn -= 1
+  },
+  'ArrowRight' : () => {
+    const nextPosition = map[playerLine][playerColumn+1];
+    if (nextPosition === undefined || nextPosition === 'W') {return}
+    playerColumn += 1
+  }
+}
+
+function appendPlayer() {
+  const currentDiv = document.getElementById(`div${playerLine}${playerColumn}`)
+  currentDiv.appendChild(document.getElementById('player'))
+}
+
+function checkWin() {
+  if (map[playerLine][playerColumn] === 'F') {
+    document.getElementById('win').classList.remove('hidden')
+  }
+}
+
+document.addEventListener('keydown', (event) => {
+  if (map[playerLine][playerColumn] !== 'F') movePlayer[event.key]()
+  appendPlayer()
+  checkWin()
+});
+
+document.getElementById('win').addEventListener('click', () => {
+  const startingPosition = document.getElementById('div90');
+  startingPosition.appendChild(player)
+  playerLine = 9;
+  playerColumn = 0;
+  document.getElementById('win').classList.add('hidden')
+})
